e6ccd7cabcac97b2c7e2ff604232203ca9c3f51b
Alejandro Avendaño <alex.aven.luna@gmail.com>
2023-03-23 10:25:12 -0600

    componentes

---
+++ lib/providers/menu_providers.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class _MenuProvider {
+  List<dynamic> opciones = [];
+
+  _MenuProvider() {
+    //loadData();
+  }
+
+  Future<List<dynamic>> loadData() async {
+    final resuesta = await rootBundle.loadString('data/menu_opciones.json');
+    Map dataMap = json.decode(resuesta);
+    //print(dataMap['rutas']);
+    opciones = dataMap['rutas'];
+    return opciones;
+  }
+
+  // loadData() {
+  //   rootBundle.loadString('data/menu_opciones.json').then((data) {
+  //     //print(data);
+
+  //     Map dataMap = json.decode(data);
+  //     print(dataMap);
+  //     print(dataMap['nombreApp']);
+  //     print(dataMap['iconApp']);
+  //   });
+  // }
+}
+
+final menuProvider = _MenuProvider();
--- lib/ui/screens/home_page_temp.dart
+++ lib/ui/screens/home_page_temp.dart
@@ -1,4 +1,8 @@
+import 'package:componentes/providers/menu_providers.dart';
+import 'package:componentes/ui/screens/alerts_screen.dart';
 import 'package:flutter/material.dart';
+
+import '../../utils/icons_mapping.dart';
 
 class HomePageTemp extends StatelessWidget {
   const HomePageTemp({super.key});
@@ -7,32 +11,82 @@ class HomePageTemp extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Componentes de Flutter'),
+        title: const Text('App de componentes'),
       ),
-      body: ListView(
-        children: const [
-          ListTile(
-            title: Text('Trivium'),
-            subtitle: Text('The Phalanx'),
-          ),
-          Divider(
-            thickness: 1.5,
-            color: Colors.blueGrey,
-          ),
-          ListTile(
-            title: Text('Onslaught'),
-            subtitle: Text('6 6 Fucking 6'),
-          ),
-          Divider(
-            thickness: 1.5,
-            color: Colors.blueGrey,
-          ),
-          ListTile(
-            title: Text('Annihilator'),
-            subtitle: Text('No Way Out'),
-          ),
-        ],
-      ),
+      body: _lista(),
     );
   }
+}
+
+Widget _lista() {
+  /*recuperacion de dtaos de un archivo json
+  //print(menuProvider.opciones);
+  menuProvider.loadData().then((opciones){
+    print('_lista: ');
+    print (opciones);
+  });
+  return ListView(
+        children: _listaItems(),
+      );*/
+  return FutureBuilder(
+    future: menuProvider.loadData(), // Indicando lo que queremos esperar
+    initialData: const [],
+    builder: (BuildContext context, AsyncSnapshot<List<dynamic>> sanpshot) {
+      //print('buldier: ');
+      //print(sanpshot.data);
+      return ListView(
+        children: _listaItems(sanpshot.data as List<dynamic>, context),
+      );
+    },
+  );
+}
+
+List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
+  final List<Widget> opciones = [];
+  IconMapping iconMap = const IconMapping();
+
+  data.forEach((opt) {
+    final title = ListTile(
+      title: Text(opt['texto']),
+      leading: iconMap.getIcon(opt['icon']),
+      trailing: const Icon(
+        Icons.keyboard_arrow_right,
+        color: Colors.blue,
+      ),
+      subtitle: Text(opt['texto2']),
+      onTap: () {
+        //final route = MaterialPageRoute(builder: (context) {
+        //return const AlertsScreen();
+        //});
+        Navigator.pushNamed(context, opt['ruta']);
+        // Navigator.push(context, route);
+      },
+    );
+    opciones
+      ..add(title)
+      ..add(const Divider());
+  });
+  return opciones;
+  // return const [
+  //   ListTile(
+  //     title: Text('Trivium'),
+  //     subtitle: Text('The Phalanx'),
+  //   ),
+  //   Divider(
+  //     thickness: 1.5,
+  //     color: Colors.blueGrey,
+  //   ),
+  //   ListTile(
+  //     title: Text('Onslaught'),
+  //     subtitle: Text('6 6 Fucking 6'),
+  //   ),
+  //   Divider(
+  //     thickness: 1.5,
+  //     color: Colors.blueGrey,
+  //   ),
+  //   ListTile(
+  //     title: Text('Annihilator'),
+  //     subtitle: Text('No Way Out'),
+  //   ),
+  // ];
 }
